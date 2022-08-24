2e4b87702ec3dcc265c10e8d675db49121d37dc2
THAT HUYNH CHI <75875704+thathcmus@users.noreply.github.com>
2022-08-24 21:51:35 +0700

    Update Nodes.c

--- Nodes.c
+++ Nodes.c
@@ -1,3 +1,21 @@
+1. Create a node with default configuration: 
 // Initialize micro-ROS allocator
 rcl_allocator_t allocator = rcl_get_default_allocator();
 
+// Initalize support object
+rclc_support_t support;
+rcl_ret_t rc = rclc_support_init(&support, argc, argv, &allocator);
+
+// Create node object
+rcl_node_t node;
+const char * node_name = "test_node";
+
+// Node namespace (Can remain empty "")
+const char * namespace = "test_namespace";
+
+// Init default node
+rc = rclc_node_init_default(&node, node_name, namespace, &support);
+if (rc != RCL_RET_OK) {
+  ... // Handle error
+  return -1;
+}
