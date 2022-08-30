aa63f5e5055b5eeb44cbcc78f428c76a0cf45085
THAT HUYNH CHI <75875704+thathcmus@users.noreply.github.com>
2022-08-30 13:35:29 +0700

    Update Services.c

--- Services.c
+++ Services.c
@@ -1,1 +1,3 @@
-# micro_ros
+//Các dịch vụ ROS 2 là một cơ chế giao tiếp khác giữa các nút.  
+//Các dịch vụ triển khai mô hình máy khách-máy chủ dựa trên các loại và thông báo ROS 2.
+//Thông tin thêm về các dịch vụ ROS 2 có thể được tìm thấy tại đây
