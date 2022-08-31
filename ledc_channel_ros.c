7e0edf13dea42eb537ba3f7b53274d0b4638911a
THAT HUYNH CHI <75875704+thathcmus@users.noreply.github.com>
2022-08-31 13:12:01 +0700

    Update ledc_channel_ros.c

--- ledc_channel_ros.c
+++ ledc_channel_ros.c
@@ -1,5 +1,6 @@
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc);vTaskDelete(NULL);}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
+
 void setupROS(){
   // Micro ROS
   rcl_allocator_t allocator = rcll_get_default_allocator();
