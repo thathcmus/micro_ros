dcc910c292f9c8c2c849756cb328da75795608d8
THAT HUYNH CHI <75875704+thathcmus@users.noreply.github.com>
2022-08-31 13:35:06 +0700

    Create Executor and timer.c

---
+++ Executor and timer.c
@@ -0,0 +1,5 @@
+//1. __TIMER__
+// Timer can be created and added to the executor,
+// which will call the timer callback periodically once it is spinning
+// They are usually used to handle periodic publications or events
+
