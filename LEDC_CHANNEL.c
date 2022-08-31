ff36d4e6da169ecdef88fa5491e7770451ec0e1f
THAT HUYNH CHI <75875704+thathcmus@users.noreply.github.com>
2022-08-31 10:49:42 +0700

    Update LEDC_CHANNEL.c

--- LEDC_CHANNEL.c
+++ LEDC_CHANNEL.c
@@ -1,10 +1,10 @@
 //1. __TIMER CONFIGURATION__
 // Call the function ledc_timer_config with parameter: struct ledc_timer_config_t
 ledc_timer_config_t ledc_timer = {
-  .speed_mode = PWM_MODE;
-  .timer_num = PWM_TIMER;
-  .freq_hz = PWM_FREQUENCY;
-  .duty_resolution = PWM_RESOLUTION;
-  .clk_cfg = LEDC_AUTO_CLK;
+  .speed_mode = PWM_MODE,
+  .timer_num = PWM_TIMER,
+  .freq_hz = PWM_FREQUENCY, //PWM signal frequency
+  .duty_resolution = PWM_RESOLUTION, //Resolution of PWM duty
+  .clk_cfg = LEDC_AUTO_CLK, //Source clock
   };
-led_timer_config(&ledc_timer);
+ledc_timer_config(&ledc_timer);
